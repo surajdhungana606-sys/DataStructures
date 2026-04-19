c1455777ca6b527543284faed4a7998db9441d2e
surajdhungana606-sys <surajdhungana606@gmail.com>
2026-04-20 00:27:45 +0545

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace DataStructures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+        }
+    }
+}
