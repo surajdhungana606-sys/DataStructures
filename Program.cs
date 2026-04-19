63bcad198dfb6419ada83282263389d4fbeeb3eb
surajdhungana606-sys <surajdhungana606@gmail.com>
2026-04-20 00:31:39 +0545

    Initial commit

--- Program.cs
+++ Program.cs
@@ -6,6 +6,10 @@
         {
             Console.WriteLine("Hello, World!");
             Console.ReadKey();
+
+            float someFloat = 3.14159f;
+            Console.WriteLine(someFloat.ToString("0.000"));
+            Console.ReadKey();
         }
     }
 }
